77bf8a39b81d1d9f52215d27db807054a21f2dff
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-02-21 14:11:45 +0100

    use laravel-vue-i18n instead of vue-i18n + configure app for slovak locale

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Floating Arboretum</title>
     <meta name="description"
-        content="Floating Arboretum je dielo Ota Hudeca prezentované v českom a slovenskom pavilóne na 60. medzinárodnej výstave umenia La Biennale di Venezia: Stranieri Ovunque - Foreigners Everywhere.">
+        content="{{ __('Floating Arboretum is a work by Ota Hudec presented in the Czech and Slovak pavilion at the 60th International Art Exhibition La Biennale di Venezia: Stranieri Ovunque - Foreigners Everywhere.') }}">
     <meta property="og:image" content="{{ asset('ogimage.png') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
