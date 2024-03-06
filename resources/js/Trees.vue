2e107bc23aaddb4a2fb24b80c2ed41188da8ad64
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2024-03-06 13:52:06 +0100

    feat: add vue router

---
+++ resources/js/Trees.vue
@@ -0,0 +1,1 @@
+<template>Trees</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,16 +2,33 @@ import "./bootstrap";
 
 import { createApp } from "vue";
 import { i18nVue } from "laravel-vue-i18n";
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "./Home.vue";
+import Detail from "./Detail.vue";
+import Trees from "./Trees.vue";
 
 import App from "./App.vue";
 
 const app = createApp(App);
 
+const routes = [
+    { path: "/", component: Home },
+    { path: "/trees/:id", component: Detail },
+    { path: "/trees", component: Trees },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+app.use(router);
 app.use(i18nVue, {
-    resolve: async lang => {
-        const langs = import.meta.glob('../../lang/*.json');
+    resolve: async (lang) => {
+        const langs = import.meta.glob("../../lang/*.json");
         return await langs[`../../lang/${lang}.json`]();
-    }
-})
+    },
+});
 
 app.mount("#app");
