bc1cae7ac5429879af5aad922d8681960c3cd291
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-03-13 14:03:15 +0100

    fix accordion

--- resources/js/components/ArtistItem.vue
+++ resources/js/components/ArtistItem.vue
@@ -7,7 +7,7 @@
             <div class="ml-4 shrink-0 w-9 h-9 bg-blue-lighter/60 group-hover:bg-blue-lighter/40 transition-colors rounded-full flex items-center justify-center">
                 <svg class="w-6 h-6 fill-none stroke-blue-darker" viewBox="0 0 24 24">
                     <path d="M4 12L20 12" stroke-linecap="square" stroke-linejoin="round" />
-                    <path v-if="expanded" d="M12 4V20" stroke-linecap="square" stroke-linejoin="round" />
+                    <path v-if="!expanded" d="M12 4V20" stroke-linecap="square" stroke-linejoin="round" />
                 </svg>
             </div>
         </button>
