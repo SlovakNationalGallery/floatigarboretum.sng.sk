674fb362507e1f177c825e1547ce972e442dd72c
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-03-21 06:32:30 +0100

    centered scrollable arboretum

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -14,67 +14,64 @@
             </h1>
         </div>
     </div>
-    <ScrollAreaRoot>
-        <ScrollAreaViewport>
-            <div class="h-96 w-[1440px] relative" ref="arboretumContainer">
-                <img
-                    src="./assets/trees/tree-05.png"
-                    srcset="./assets/trees/tree-05.png 1x, ./assets/trees/tree-05@2x.png 2x"
-                    class="absolute top-[76px] left-[49px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-10.png"
-                    srcset="./assets/trees/tree-10.png 1x, ./assets/trees/tree-10@2x.png 2x"
-                    class="absolute top-[136px] left-[220px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-06.png"
-                    srcset="./assets/trees/tree-06.png 1x, ./assets/trees/tree-06@2x.png 2x"
-                    class="absolute top-[159px] left-[295px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float1 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-01.png"
-                    srcset="./assets/trees/tree-01.png 1x, ./assets/trees/tree-01@2x.png 2x"
-                    class="absolute top-[67px] left-[396px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-08.png"
-                    srcset="./assets/trees/tree-08.png 1x, ./assets/trees/tree-08@2x.png 2x"
-                    class="absolute -top-[90px] left-[517px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-04.png"
-                    srcset="./assets/trees/tree-04.png 1x, ./assets/trees/tree-04@2x.png 2x"
-                    class="absolute top-[136px] left-[678px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-03.png"
-                    srcset="./assets/trees/tree-03.png 1x, ./assets/trees/tree-03@2x.png 2x"
-                    class="absolute top-[77px] left-[821px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-02.png"
-                    srcset="./assets/trees/tree-02.png 1x, ./assets/trees/tree-02@2x.png 2x"
-                    class="absolute top-[91px] left-[954px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-07.png"
-                    srcset="./assets/trees/tree-07.png 1x, ./assets/trees/tree-07@2x.png 2x"
-                    class="absolute -top-[18px] left-[1106px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
-                />
-                <img
-                    src="./assets/trees/tree-09.png"
-                    srcset="./assets/trees/tree-09.png 1x, ./assets/trees/tree-09@2x.png 2x"
-                    class="absolute top-[91px] left-[1251px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
-                />
-            </div>
-        </ScrollAreaViewport>
-        <ScrollAreaScrollbar orientation="horizontal" class="w-full h-2 bg-white/30" v-if="useHorizontalScroll">
-            <ScrollAreaThumb />
-        </ScrollAreaScrollbar>
-    </ScrollAreaRoot>
+    <div class="w-screen overflow-x-auto overflow-y-visible h-[500px] z-10 absolute top-52" ref="arboretumWrapper">
+        <div class="h-96 w-[1440px] mx-auto relative z-10" ref="arboretumContainer">
+            <img
+                src="./assets/trees/tree-05.png"
+                srcset="./assets/trees/tree-05.png 1x, ./assets/trees/tree-05@2x.png 2x"
+                class="absolute top-[176px] left-[49px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-10.png"
+                srcset="./assets/trees/tree-10.png 1x, ./assets/trees/tree-10@2x.png 2x"
+                class="absolute top-[236px] left-[220px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-06.png"
+                srcset="./assets/trees/tree-06.png 1x, ./assets/trees/tree-06@2x.png 2x"
+                class="absolute top-[259px] left-[295px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float1 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-01.png"
+                srcset="./assets/trees/tree-01.png 1x, ./assets/trees/tree-01@2x.png 2x"
+                class="absolute top-[167px] left-[396px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-08.png"
+                srcset="./assets/trees/tree-08.png 1x, ./assets/trees/tree-08@2x.png 2x"
+                class="absolute top-[10px] left-[517px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-04.png"
+                srcset="./assets/trees/tree-04.png 1x, ./assets/trees/tree-04@2x.png 2x"
+                class="absolute top-[236px] left-[678px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-03.png"
+                srcset="./assets/trees/tree-03.png 1x, ./assets/trees/tree-03@2x.png 2x"
+                class="absolute top-[177px] left-[821px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-02.png"
+                srcset="./assets/trees/tree-02.png 1x, ./assets/trees/tree-02@2x.png 2x"
+                class="absolute top-[191px] left-[954px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-07.png"
+                srcset="./assets/trees/tree-07.png 1x, ./assets/trees/tree-07@2x.png 2x"
+                class="absolute top-[82px] left-[1106px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float2 hover:pause"
+            />
+            <img
+                src="./assets/trees/tree-09.png"
+                srcset="./assets/trees/tree-09.png 1x, ./assets/trees/tree-09@2x.png 2x"
+                class="absolute top-[201px] left-[1251px] drop-shadow-lg hover:cursor-pointer hover:opacity-90 transition hover:drop-shadow-2xl animate-float3 hover:pause"
+            />
+        </div>
+    </div>
+    <div class="h-96"></div>
+
     <!-- <img class="pb-12 object-cover h-96 md:mx-auto md:-mt-16" src="./assets/arboretum.png" /> -->
-  
+
     <div class="container mx-auto px-6 py-12 md:flex">
         <article>
             <h2 class="text-lg pb-2 opacity-60">
@@ -95,7 +92,9 @@
             </router-link>
         </article>
         <div class="flex justify-center items-center flex-1 pt-12 md:pt-0">
-            <span class="text-center inline-block max-w-80 py-3 px-8 md:text-2xl bg-orange -rotate-12 hover:-rotate-6 text-blue-darker transition rounded-[70px]">
+            <span
+                class="text-center inline-block max-w-80 py-3 px-8 md:text-2xl bg-orange -rotate-12 hover:-rotate-6 text-blue-darker transition rounded-[70px]"
+            >
                 {{ $t("The exhibition starts on the 20th of April") }}
             </span>
         </div>
@@ -108,6 +107,7 @@ import { ScrollAreaRoot, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewpor
 
 const arboretumContainer = ref(null);
 const useHorizontalScroll = ref(false);
+const arboretumWrapper = ref(null);
 
 onMounted(() => {
     setOffset();
@@ -115,13 +115,15 @@ onMounted(() => {
 });
 
 const setOffset = () => {
-    if (arboretumContainer.value.offsetWidth < 1440) {
+    if (arboretumWrapper.value.offsetWidth < 1440) {
         useHorizontalScroll.value = true;
-        const offset = (1440 - arboretumContainer.value.offsetWidth) / 2;
-        arboretumContainer.value.style.left = `-${offset}px`;
+        const offset = (1440 - arboretumWrapper.value.offsetWidth) / 2;
+        arboretumWrapper.value.scrollLeft = offset;
+        console.log(arboretumWrapper.value.offsetWidth);
     } else {
         useHorizontalScroll.value = false;
-        arboretumContainer.value.style.left = "0px";
+        arboretumWrapper.value.scrollLeft = "0px";
+        console.log(arboretumWrapper.value.offsetWidth);
     }
 };
 </script>
