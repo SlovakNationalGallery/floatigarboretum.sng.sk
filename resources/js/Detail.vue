56fad4d9da4c7975c730e67a105f486b9843f6c3
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2024-04-05 13:57:32 +0200

    feat: add transitions

--- resources/js/Detail.vue
+++ resources/js/Detail.vue
@@ -5,17 +5,33 @@
                 class="flex flex-col items-center justify-center overflow-hidden relative mt-16 lg:sticky lg:top-0 lg:left-16 lg:p-4"
             >
                 <div class="flex relative items-center w-[175%] gap-2">
-                    <img
-                        class="w-1/2 h-80 object-bottom py-3 object-contain lg:invisible animate-float2"
-                        :src="state.imageSrcPrev"
-                        :srcset="`${state.imageSrcsetPrev}`"
-                    />
-                    <img class="w-2/3 h-96 object-bottom object-contain animate-float1" :src="state.imageSrc" :srcset="`${state.imageSrcset}`" />
-                    <img
-                        class="w-1/2 h-80 object-bottom py-3 object-contain lg:invisible animate-float3"
-                        :src="state.imageSrcNext"
-                        :srcset="`${state.imageSrcsetNext}`"
-                    />
+                    <DetailTransition :right-origin="state.direction === 'right'" zoom="in">
+                        <div class="w-1/2 h-80" :key="state.index">
+                            <img
+                                class="w-full h-full object-bottom object-contain lg:invisible animate-float2"
+                                :src="state.imageSrcPrev"
+                                :srcset="`${state.imageSrcsetPrev}`"
+                            />
+                        </div>
+                    </DetailTransition>
+                    <DetailTransition :right-origin="state.direction === 'right'" zoom="out">
+                        <div class="w-2/3 h-96" :key="state.index">
+                            <img
+                                class="w-full h-full object-bottom object-contain animate-float1"
+                                :src="state.imageSrc"
+                                :srcset="`${state.imageSrcset}`"
+                            />
+                        </div>
+                    </DetailTransition>
+                    <DetailTransition :right-origin="state.direction === 'right'" zoom="in">
+                        <div class="w-1/2 h-80" :key="state.index">
+                            <img
+                                class="w-full h-full object-bottom object-contain lg:invisible animate-float3"
+                                :src="state.imageSrcNext"
+                                :srcset="`${state.imageSrcsetNext}`"
+                            />
+                        </div>
+                    </DetailTransition>
                 </div>
                 <router-link
                     :to="{ name: 'tree', params: { id: state.indexPrev } }"
@@ -39,62 +55,89 @@
                         ></path>
                     </svg>
                 </router-link>
-                <h2 class="font-display text-3xl pt-4 text-center">{{ trees[state.index - 1][lang].title }}</h2>
-                <span class="text-sm pb"
-                    >{{ trees[state.index - 1][lang].location }}{{
-                        trees[state.index - 1][lang].protest && `/${trees[state.index - 1][lang].protest}`
-                    }}{{ trees[state.index - 1][lang].dating && `/${trees[state.index - 1][lang].dating}` }}</span
-                >
-                <div class="hidden pt-10 left-0 right-0 lg:block w-full">
-                    <MusicPlayer class="container mx-auto" :index="state.index" :key="state.index"></MusicPlayer>
-                </div>
+                <FadeInOutTransition>
+                    <div :key="state.index" class="w-full text-center py-3">
+                        <h2 class="font-display text-3xl pt-4 text-center">{{ trees[state.index - 1][lang].title }}</h2>
+                        <span class="text-sm pb"
+                            >{{ trees[state.index - 1][lang].location
+                            }}{{ trees[state.index - 1][lang].protest && `/${trees[state.index - 1][lang].protest}`
+                            }}{{
+                                trees[state.index - 1][lang].dating && `/${trees[state.index - 1][lang].dating}`
+                            }}</span
+                        >
+                        <div class="hidden pt-10 left-0 right-0 lg:block w-full">
+                            <MusicPlayer
+                                class="container mx-auto"
+                                :index="state.index"
+                                :key="state.index"
+                            ></MusicPlayer>
+                        </div>
+                    </div>
+                </FadeInOutTransition>
             </section>
         </div>
-        <div class="lg:relative lg:w-full lg:mt-96">
-            <section class="container mx-auto px-5 mt-6 lg:mr-0">
-                <h3 class="text-xl opacity-60 pb-2">{{ $t("Story") }}</h3>
-                <p class="pb-8 lg:text-2xl whitespace-pre-wrap" v-html="trees[state.index - 1][lang].story"></p>
-                <h3 class="text-xl opacity-60 pb-2">{{ $t("Poem") }}</h3>
-                <p class="lg:text-xl whitespace-pre-wrap">{{ trees[state.index - 1].poem }}</p>
-                <p class="text-sm pt-3 lg:text-xl" v-html="trees[state.index - 1].references"></p>
-            </section>
-            <div class="sticky container mx-auto px-5 bottom-7 pb-2.5 pt-10 left-0 right-0 lg:hidden">
-                <MusicPlayer :index="state.index" :key="state.index"></MusicPlayer>
-            </div>
-            <section class="flex flex-col container mx-auto px-5 gap-2.5 lg:mr-0 lg:flex-row lg:py-20">
-                <!-- TODO: Add proper links -->
-                <!-- Hidden for now -->
-                <!-- <router-link
+        <FadeInOutTransition>
+            <div class="lg:relative lg:w-full lg:mt-96" :key="state.index">
+                <section class="container mx-auto px-5 mt-6 lg:mr-0">
+                    <h3 class="text-xl opacity-60 pb-2">{{ $t("Story") }}</h3>
+                    <p class="pb-8 lg:text-2xl whitespace-pre-wrap" v-html="trees[state.index - 1][lang].story"></p>
+                    <h3 class="text-xl opacity-60 pb-2">{{ $t("Poem") }}</h3>
+                    <p class="lg:text-xl whitespace-pre-wrap">{{ trees[state.index - 1].poem }}</p>
+                    <p class="text-sm pt-3 lg:text-xl" v-html="trees[state.index - 1].references"></p>
+                </section>
+                <div
+                    class="sticky container mx-auto px-5 bottom-7 pb-2.5 pt-10 left-0 right-0 lg:hidden"
+                    :key="state.index"
+                >
+                    <MusicPlayer :index="state.index"></MusicPlayer>
+                </div>
+                <section class="flex flex-col container mx-auto px-5 gap-2.5 lg:mr-0 lg:flex-row lg:py-20">
+                    <!-- TODO: Add proper links -->
+                    <!-- Hidden for now -->
+                    <!-- <router-link
                     to="/about"
                     class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px] text-center"
                 >
                     {{ $t("Discover visitors stories") }}
                 </router-link> -->
-                <router-link
-                    to="/about"
-                    class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px] text-center"
-                >
-                    {{ $t("Submit your story") }}
-                </router-link>
-            </section>
-        </div>
+                    <router-link
+                        to="/about"
+                        class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px] text-center"
+                    >
+                        {{ $t("Submit your story") }}
+                    </router-link>
+                </section>
+            </div>
+        </FadeInOutTransition>
     </div>
 </template>
 <script setup>
 import MusicPlayer from "./MusicPlayer.vue";
 import { trees } from "./consts";
 import { useRoute } from "vue-router";
-import { watch, reactive } from "vue";
+import { watch, reactive, ref } from "vue";
 import { useLanguage } from "./composables/language";
+import DetailTransition from "./components/DetailTransition.vue";
+import FadeInOutTransition from "./components/FadeInOutTransition.vue";
 
 const route = useRoute();
-const state = reactive({ index: 0, indexNext: 0, indexPrev: 0, imageUrl: "", imageUrlNext: "", imageUrlPrev: "" });
+const state = reactive({
+    index: 0,
+    indexNext: 0,
+    indexPrev: 0,
+    imageUrl: "",
+    imageUrlNext: "",
+    imageUrlPrev: "",
+    direction: null,
+});
+const oldRouteIndex = ref();
 const { lang } = useLanguage();
 
 watch(
     route,
     () => {
-        state.index = parseInt(route.params.id) || 1;
+        const newIndex = parseInt(route.params.id) || 1;
+        state.index = newIndex;
         state.indexNext = !parseInt(route.params.id) || state.index > trees.length - 1 ? 1 : state.index + 1;
         state.indexPrev = !parseInt(route.params.id) || state.index < 2 ? trees.length : state.index - 1;
 
@@ -116,6 +159,19 @@ watch(
             " 1x, " +
             new URL(`./assets/trees/tree-${state.indexPrev}@2x.png`, import.meta.url).href +
             " 2x";
+
+        if (!oldRouteIndex) return;
+        if (newIndex === 1 && oldRouteIndex.value === trees.length) {
+            state.direction = "right";
+        } else if (newIndex === trees.length && oldRouteIndex.value === 1) {
+            state.direction = "left";
+        } else if (newIndex > oldRouteIndex.value) {
+            state.direction = "right";
+        } else {
+            state.direction = "left";
+        }
+
+        oldRouteIndex.value = newIndex;
     },
     { immediate: true }
 );
---
+++ resources/js/components/DetailTransition.vue
@@ -0,0 +1,25 @@
+<template>
+    <Transition
+        mode="out-in"
+        enter-active-class="duration-300 ease-linear"
+        :enter-from-class="`transform ${
+            rightOrigin ? 'translate-x-1/2' : '-translate-x-1/2'
+        } lg:translate-x-0 lg:opacity-0 ${zoom === 'in' ? 'scale-[1.075]' : ''} ${
+            zoom === 'out' ? 'scale-[0.9025]' : ''
+        }`"
+        leave-active-class="duration-300 ease-linear"
+        enter-to-class="opacity-100"
+        leave-from-class="opacity-100"
+        :leave-to-class="`transform ${
+            rightOrigin ? '-translate-x-1/2' : 'translate-x-1/2'
+        } lg:translate-x-0 lg:opacity-0 ${zoom === 'in' ? 'scale-[1.075]' : ''} ${
+            zoom === 'out' ? 'scale-[0.9025]' : ''
+        }`"
+    >
+        <slot></slot>
+    </Transition>
+</template>
+
+<script setup>
+const props = defineProps(["rightOrigin", "zoom"]);
+</script>
