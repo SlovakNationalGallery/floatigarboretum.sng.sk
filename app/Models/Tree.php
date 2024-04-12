81b259ceaaf6bd5b758c6a00cd0ce83d60704200
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-12 10:57:09 +0200

    prepare Tree model

--- app/Models/Tree.php
+++ app/Models/Tree.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Tree extends Model
+class Tree extends Model implements HasMedia
 {
     use HasFactory;
     use HasTranslations;
+    use InteractsWithMedia;
 
     public $translatable = ['title', 'location', 'story'];
     protected $fillable = ['title', 'location', 'story', 'dating', 'estimated_age', 'estimated_height', 'is_published'];
