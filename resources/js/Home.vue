c6caed2ec3eac6d642afd6ceea56422abac5a3ab
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-12 16:33:53 +0200

    add route to tree detail

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -20,56 +20,67 @@
     >
         <div class="h-96 w-[1440px] mx-auto relative z-10" ref="arboretumContainer">
             <img
+                @click="selectTree(6)"
                 src="./assets/trees/tree-6.png"
                 srcset="./assets/trees/tree-6.png 1x, ./assets/trees/tree-6@2x.png 2x"
                 class="absolute top-[141px] left-[51px] drop-shadow-lg animate-float2 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(1)"
                 src="./assets/trees/tree-1.png"
                 srcset="./assets/trees/tree-1.png 1x, ./assets/trees/tree-1@2x.png 2x"
                 class="absolute top-[203px] left-[174px] drop-shadow-lg animate-float3 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(2)"
                 src="./assets/trees/tree-2.png"
                 srcset="./assets/trees/tree-2.png 1x, ./assets/trees/tree-2@2x.png 2x"
                 class="absolute top-[194px] left-[296px] drop-shadow-lg animate-float1 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(5)"
                 src="./assets/trees/tree-5.png"
                 srcset="./assets/trees/tree-5.png 1x, ./assets/trees/tree-5@2x.png 2x"
                 class="absolute top-[213px] left-[409px] drop-shadow-lg animate-float2 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(7)"
                 src="./assets/trees/tree-7.png"
                 srcset="./assets/trees/tree-7.png 1x, ./assets/trees/tree-7@2x.png 2x"
                 class="absolute top-[10px] left-[534px] drop-shadow-lg animate-float3 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(8)"
                 src="./assets/trees/tree-8.png"
                 srcset="./assets/trees/tree-8.png 1x, ./assets/trees/tree-8@2x.png 2x"
                 class="absolute top-[213px] left-[685px] drop-shadow-lg animate-float3 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(9)"
                 src="./assets/trees/tree-9.png"
                 srcset="./assets/trees/tree-9.png 1x, ./assets/trees/tree-9@2x.png 2x"
                 class="absolute top-[110px] left-[798px] drop-shadow-lg animate-float2 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(1)"
                 src="./assets/trees/tree-11.png"
                 srcset="./assets/trees/tree-11.png 1x, ./assets/trees/tree-11@2x.png 2x"
                 class="absolute top-[202px] left-[918px] drop-shadow-lg animate-float3 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(1)"
                 src="./assets/trees/tree-10.png"
                 srcset="./assets/trees/tree-10.png 1x, ./assets/trees/tree-10@2x.png 2x"
                 class="absolute top-[163px] left-[975px] drop-shadow-lg animate-float2 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(4)"
                 src="./assets/trees/tree-4.png"
                 srcset="./assets/trees/tree-4.png 1x, ./assets/trees/tree-4@2x.png 2x"
                 class="absolute top-[112px] left-[1105px] drop-shadow-lg animate-float3 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
             />
             <img
+                @click="selectTree(3)"
                 src="./assets/trees/tree-3.png"
                 srcset="./assets/trees/tree-3.png 1x, ./assets/trees/tree-3@2x.png 2x"
                 class="absolute top-[151px] left-[1221px] drop-shadow-lg animate-float1 hover:cursor-pointer hover:pause hover:drop-shadow-2xl transition-shadow"
@@ -92,19 +103,19 @@
                 }}
             </p>
             <div class="flex flex-col container mx-auto gap-2.5 lg:mr-0 lg:flex-row">
-            <router-link
-                to="/about"
-                class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px]"
-            >
-                {{ $t("Find out more") }}
-            </router-link>
-            <a
-                href="https://jira.sng.sk/servicedesk/customer/portal/9/create/98"
-                class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px]"
-            >
-                {{ $t("Submit your story") }}
-            </a>
-        </div>
+                <router-link
+                    to="/about"
+                    class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px]"
+                >
+                    {{ $t("Find out more") }}
+                </router-link>
+                <a
+                    href="https://jira.sng.sk/servicedesk/customer/portal/9/create/98"
+                    class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition rounded-[70px]"
+                >
+                    {{ $t("Submit your story") }}
+                </a>
+            </div>
         </article>
         <div class="flex justify-center items-center flex-1 pt-12 md:pt-0">
             <router-link
@@ -119,7 +130,9 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const arboretumContainer = ref(null);
 const arboretumWrapper = ref(null);
 
@@ -133,5 +146,9 @@ const adjustScroll = () => {
     arboretumWrapper.value.scrollLeft = useHorizontalScroll
         ? (arboretumContainer.value.offsetWidth - arboretumWrapper.value.offsetWidth) / 2
         : 0;
+};
+
+const selectTree = (id) => {
+    router.push(`/trees/${id}`);
 };
 </script>
