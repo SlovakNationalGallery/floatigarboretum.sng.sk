69071b2a072abf3307cf4e9baa48a156e6326299
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-12 15:32:36 +0200

    if the user comes from a QR code, switch to English language automatically (if language is not set yet)

--- resources/js/composables/language.js
+++ resources/js/composables/language.js
@@ -1,7 +1,7 @@
-import { ref, watch, toRef } from "vue";
-import { loadLanguageAsync } from "laravel-vue-i18n";
+import { ref, watch, toRef, onMounted } from "vue";
+import { getActiveLanguage, loadLanguageAsync } from "laravel-vue-i18n";
 
-const lang = ref(sessionStorage.getItem("language") || "sk");
+const lang = ref(localStorage.getItem("language") || "sk");
 
 export function useLanguage() {
 
@@ -14,6 +14,19 @@ export function useLanguage() {
     watch(lang, (newVal, oldVal) => {
         if (newVal !== oldVal) {
             console.log(`Language changed from ${oldVal} to ${newVal}`);
+        }
+    });
+
+    onMounted(() => {
+        // If the user comes from a QR code, switch to English language automatically (if language is not set yet)
+        const urlParams = new URLSearchParams(window.location.search);
+        if ((urlParams.has('utm_source') && urlParams.get('utm_source') === 'qr') && !localStorage.getItem("language")) {
+            lang.value = 'en';
+            localStorage.setItem("language", lang.value);
+        }
+
+        if (lang.value !== getActiveLanguage()) {
+            loadLanguageAsync(lang.value);
         }
     });
 
