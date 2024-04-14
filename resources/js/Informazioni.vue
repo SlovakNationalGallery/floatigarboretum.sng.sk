d775c484d69947a545408f6a4413004f756e92b6
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-14 15:45:43 +0200

    add inforazioni component + route

---
+++ resources/js/Informazioni.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="mx-auto container px-5 h-60 flex items-end">
+        <h1 class="font-display text-4xl md:text-5xl text-blue-dark tracking-wide max-w-48 md:max-w-full">
+            Oto Hudec<br />
+            Arboreto galleggiante
+        </h1>
+    </div>
+    <div class="container mx-auto px-6 py-12 md:pt-16">
+        <article>
+            <h2 class="text-lg pb-2 opacity-60">Informazioni sul progetto</h2>
+            <p class="pb-6 max-w-[800px] leading-normal md:text-2xl">
+                Padiglione ceco e slovacco<br />
+                60a Esposizione Internazionale d'Arte<br />
+                la Biennale di Venezia<br />
+                20 aprile - 24 novembre 2024<br /><br />
+
+                Curatore: Lýdia Pribišová<br />
+                Commissario: Monika Krčmárik<br />
+            </p>
+        </article>
+
+        <article>
+            <p class="pb-6 max-w-prose">
+                Immaginate tutto ciò che potremmo realizzare insieme.<br />Immaginate tutte le cose che potremmo fare
+                insieme, invece di tagliare vite, invece di sfrattare sogni. Spesso agiamo l'uno contro l'altro,
+                crescita contro crescita, motoseghe e fuoco contro natura selvaggia. Ma non dimentichiamo! La foresta è
+                un organismo multispecie ed è sempre aperta all'inclusione dell'uomo nella sua comunità.
+            </p>
+            <p class="pb-6 max-w-prose">
+                Oto Hudec lavora con un archivio/database/file di storie che raccontano di uno sforzo collettivo quando
+                una comunità unita di attivisti ha impedito la deforestazione e la distruzione degli alberi. L'artista
+                stesso è un attivista e ha partecipato attivamente a diverse azioni di protesta. Crede che le storie del
+                Floating Arboretum possano accendere l'immaginazione trasformativa e che possano essere d'ispirazione
+                per la nostra vita quotidiana.
+            </p>
+            <p class="pb-6 max-w-prose">
+                Un motivo ricorrente nell'opera di Oto Hudec è l'evasione, il desiderio di salvare ciò che è quasi
+                impossibile salvare in un dato mondo, in un dato contesto di civiltà, attraverso il volo, il nuoto, le
+                navi o le navicelle spaziali. Anche gli alberi dei Giardini, o di Venezia, sono minacciati di estinzione
+                a causa dei cambiamenti climatici e dell'innalzamento del livello del mare.
+            </p>
+            <p class="pb-6 max-w-prose">
+                Oto chiede il loro salvataggio, così come quello di altri alberi in pericolo nel mondo. Concepisce un
+                arboreto, un luogo simbolico e utopico, un santuario per gli alberi minacciati dall'espansione e dalla
+                stravaganza umana. Il progetto è uno sguardo immaginifico a un futuro distopico in cui selezioniamo e
+                salviamo gli alberi trasferendoli in un arboreto (immaginario) sicuro. Qui si intersecano diverse linee
+                temporali: le proteste attuali contro la deforestazione, l'abbattimento degli alberi in diverse parti
+                del mondo, la crisi climatica e il futuro immaginario della loro salvaguardia.
+            </p>
+        </article>
+
+        <article class="mb-8">
+            <h2 class="text-lg pb-2 opacity-60">Artisti coinvolti</h2>
+            <ul class="pb-6 max-w-[800px] md:text-xl">
+                <li>Opera sonora e performance: Fero Király</li>
+                <li>Poeticizzazione di storie di alberi: Juliana Sokolová</li>
+                <li>Interpreti: Eva Šušková, Anna Čonková,</li>
+                <li>Ivanka Chrapková, Peter Mazalán, Marek Kundlák,</li>
+                <li>Vojtěch Šembera</li>
+                <li>Coreografia: Petra Fornayová</li>
+                <li>Costumista: Michaela Bednárová</li>
+                <li>Architetto: Tomáš Boroš</li>
+                <li>Costruzione e installazione della barca: Róbert Bernáth,</li>
+                <li>Sarah Hreščáková, Oto Hudec</li>
+                <li>Murale: Viktor Fehér, Michal Turkovič, Oto Hudec</li>
+                <li>Scultura in pietra di pigna: Juraj Parák, Maroš Parák</li>
+                <li>Design grafico: Samuel Čarnoký</li>
+                <li>Assistenza tecnica: Peter Beňo</li>
+                <li>Redazione: Miroslava Plesníková, Luďka Kratochvílová, Beata Bradford</li>
+                <li>
+                    Applicazione web: lab.SNG (Alena Pôbišová, Gorazd Ratulovský, Igor Rjabinin, František Michal
+                    Sebestyén)
+                </li>
+            </ul>
+        </article>
+
+        <div class="pb-10 flex flex-col gap-3 md:flex-row">
+            <router-link
+                to="/"
+                class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition text-center rounded-[70px]"
+            >
+                Ascoltare le storie degli alberi
+            </router-link>
+            <router-link
+                to="/for-visitors"
+                class="font-display py-3 px-8 text-lg border-white/30 border-2 hover:bg-orange hover:border-orange hover:text-blue-darker transition text-center rounded-[70px]"
+            >
+                Programma degli spettacoli
+            </router-link>
+        </div>
+
+        <article>
+            <p class="pb-6 max-w-prose">
+                Organizzatori:<br />
+                Galleria Nazionale Slovacca<br />
+                Ministero della Cultura della Repubblica Slovacca
+            </p>
+            <p class="pb-6 max-w-prose">
+                Realizzata in collaborazione con:<br />
+                Kunsthalle Bratislava e Galleria Gandy
+            </p>
+            <p class="pb-6 max-w-prose">
+                Partner generale:<br />
+                Tatra banka, a. s.
+            </p>
+        </article>
+    </div>
+</template>
+
+<script setup>
+
+</script>
