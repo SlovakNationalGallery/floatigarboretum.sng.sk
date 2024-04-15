f936885d2f09e9489349e74961125d59163b83ca
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-15 13:50:22 +0200

    update performers in trees

--- resources/js/consts.js
+++ resources/js/consts.js
@@ -16,7 +16,7 @@ export const trees = [
         },
         poem: "By the roadside, a woman waits for the bus: The forest she came to see, the stream, all gone. She tells of her sadness and pain to the vendor of tea.\n\nTo which the vendor of tea replies: There is hope now. The women in the valley ’ve started saying they\n\nwon’t let the trees be cut down.[1]\n\n\nThe women, they’ve started to march, and they embrace the trees, and they stay until the loggers leave\n\n\n—the entire night they kept guard of the trees, and the following morning\n\nmore villagers came, they take turns,\n\nthey rotate who’s by the trees and who’s by the children.\n\n\nTennis racquets? What the hell\n\nTennis racquets? What the hell\n\n\nIn its permanence, the forest is our economy,\n\nthe women said",
         references: "[1] based on a story told by Vandana Shiva in various interviews.",
-        performers: "Eva Šušková, Marek Kundlák",
+        performers: "Eva Šušková, Peter Mazalán",
     },
     {
         sk: {
@@ -52,7 +52,7 @@ export const trees = [
             story: "After a wind calamity in which large areas of pine forest in a Slovak national park were uprooted, the state company started to process the wood. They claimed that removal of the wood would avoid the spread of bark beetles and fires. Conservationists, however, declared that it violated nature protection laws and that they do not have authorisation to intervene in a habitat of European importance. They began a protest to protect the natural area of the Tichá and Kôprová valley from logging and harvesting of wood, and to maintain natural renewal of the forest. After police detentions and a legal battle, it was eventually decided that the Kôprová and Tichá valley would be left to renew naturally as a wild forest.\n\nThis area is home to the oldest trees in Slovakia – Arolla pines (Pinus cembra), possibly more than 1,000 years old. The Arolla pine is no ordinary tree; on the lowlands, it was displaced by other species, but under the peaks of the rocky Tatra mountains it is the only one that thrives, requiring just a little soil and moss, and even growing on rock.",
         },
         poem: "The respect one feels towards trees that bear nuts\n\n\nThe respect one feels towards trees that bear nuts\n\nat the upper limit of the forest belt\n\n\nThe respect one feels towards trees that need no soil almost,\n\ngrow on rock\n\nwithstand wind\n\nThe roots of the cembra pine stabilise           stabilise soil\n\nprotect against avalanche\n\n\nThe sky darkened as the gale gathered that November night\n\nIt was three years before the loggers arrived here, and so did we, winning time.\n\n\nLoggers at the ministry\n\nPoachers at the ministry\n\nHunters at the ministry.\n\n\n\nWe stand, we sail, we do not mourn, we curse\n\nWhat will protect you?\n\nIn vessels of stone pine the milk will not sour, the water will not spoil\n\nWe stand, we sail, we do not mourn, we curse\n\nWhat will protect you?\n\n\nLoggers at the ministry\n\nPoachers at the ministry\n\nHunters at the ministry.\n\n\n\nAt night we feel as a species again,\n\ntrapped by our shared biology.",
-        performers: "Eva Šušková, Peter Mazalán",
+        performers: "Anna Čonková, Peter Mazalán",
     },
     {
         sk: {
@@ -70,7 +70,7 @@ export const trees = [
             story: "Perrier's baobab, in Latin Adansonia Perrieri, is one of Madagascar's most endangered baobabs. Endemic to northern Madagascar, fewer than 200 of these trees remain, placing it at high risk of extinction – its inclusion on the Red List recently suggested critical endangerment. The tree faces threats from climate change and from habitat loss due to fire, logging for charcoal and timber, and clearing for mining. Environmentally damaging sapphire mines are common in this region of Madagascar.\n\nHowever sad the present story of the tree is, it is underlined by the name it acquired – Joseph Marie Henry Alfred Perrier de la Bâthie was a French botanist whose name was given to this ’discovered’ species. But he was also the man who in 1924 brought cochineal insects to Madagascar to counter strategic planting of opuntia cacti by local people. The cochineal quickly spread like a pest across the island, causing famine and wider societal changes.",
         },
         poem: "The tree called a scientific wonder\n\nthe tree called a wonder\n\n\n\nin the false cavities bark grows\n\nthirty adult people can take shelter in the trunk\n\na tree capable of engulfing\n\ncapable of living thousands of years\n\ncapable of storing water in its trunk\n\na tree under which you can boil coffee\n\na tree for which to pray\n\na tree that will pray for you\n\na tree whose flowers are pollinated by bats\n\na truly magnanimous tree\n\na tree that moves in the dark\n\na tree that has moved further and further north\n\nbut there is a limit to the north\n\n\n\nThe tree that refuses its name:\n\nthose who named it\n\nand us who choke it\n\nshare a surname",
-        performers: "Ivana Chrapková, Marek Kundlák",
+        performers: "Vojtěch Šembera",
     },
     {
         sk: {
@@ -89,7 +89,7 @@ export const trees = [
         },
         poem: "the stories a rainforest contains, the rainy place\n\nslide with the mud to cover the road newly built\n\neven in October, the driest month\n\n\nusing the weight of bodies to gain minutes, hours, months\n\nthe stories a rainforest contains, the rainy place\n\nthe deep truths sedimented in our syntax[1], we carry some of the sediment around our roots",
         references: "[1] Klaus in Ben Lerner, Topeka School (Granta, 2020)",
-        performers: "Anna Čonková, Peter Mazalán",
+        performers: "Anna Čonková",
     },
     {
         sk: {
@@ -107,7 +107,7 @@ export const trees = [
             story: "An old, large specimen of Honey Locust grows near the northern corner of the Czechoslovak pavilion. One of its main branches collapsed on the evening of 8 July 2019, causing damage to the roof of the pavilion and effectively making the entire pavilion unusable. In Venice that day, an exceptional windstorm reached up to 90 km/h. Entire trees were uprooted throughout the area. The pavilion, which was built in 1926, awaited reconstruction until 2024, when a temporary roof was eventually installed.\n\nVenice and its lagoon represent one of the areas most threatened by climate change, pollution, erosion, and subsidence. A UNESCO report from 2023 cited climate change-induced sea level rise and extreme weather, along with mass tourism and construction, as placing Venice's old buildings, trees, and landscapes at risk. The report recommended designating Venice as endangered.",
         },
         poem: "As we all know from school\n\nfor the forces of the external world\n\na building is but a postponement\n\n\nGlass\n\nshattering\n\n\nOn the night, a severe meteorological event swept through Venice,\n\na meteorological event so severe\n\ntrees, elsewhere, were uprooted\n\ngale force wind in this city\n\nwhose silence has more than once been called geological\n\n\nas for me, a branch broke,\n\n                                    crashing onto the pavilion roof\n\n\nGlass\n\nshattering\n\n\nTrees, trees have been there before me, touched the glass roof from below,\n\nearlier, a decade ago\n\n\nBut this story, story of this arboretum, begins with me and that July night,\n\na building paralysed by weather\n\nand all it could have contained\n\nfor years\n\n\nI arrived by water,\n\nI will depart by water\n\n\nEverything can be described in a way that will calm you\n\neventually",
-        performers: "Ivana Chrapková",
+        performers: "Anna Čonková, Vojtěch Šembera",
     },
     {
         sk: {
@@ -125,7 +125,7 @@ export const trees = [
             story: "This is the story of a young woman named Julia and a redwood tree. She could not bear to watch the lumber company cut down redwoods. She climbed a 1,000-year-old sequoia tree named Luna to protect it from a logging company. She remained in the tree canopy for two years. With the help of activists on the ground, she withstood freezing rain, strong winds, and intimidation by security guards and loggers. She gave interviews to the media directly from the treetop. Only when the company agreed to spare the tree and the surrounding area did Julia climb down.",
         },
         poem: "In December 1997, a woman climbed into a sequoia,\n\na thousand-year-old sequoia,\n\nmarked to be logged—to make          front yard decks\n\n                                                           back yard decks\n\n                                                           front yard fences\n\n                                                           back yard fences\n\n                                                           chairs\n\n\nThe bark of the redwood is so thick, so moist,\n\nthat the tree can be hollowed out by lighting fire and still survive\n\n\nFirst winter: wind, storms, El Nino, loggers, wind water slashing branches crashing\n\nfor 16 hours she endured the storm\n\nSecond winter: snow and ice, loneliness,\n\non a platform 50m above the ground\n\nFor two years and eight days\n\n\n— not because we want it all\n\nbut because all that’s left is so little\n\n\nthe difference: endurance\n\nthe difference: endurance",
-        performers: "Anna Čonková",
+        performers: "Eva Šušková, Marek Kundlák",
     },
     {
         sk: {
@@ -162,7 +162,7 @@ export const trees = [
         },
         poem: "They said, it was a nice forest\n\n                        old forest\n\ntwelve-thousand-year-old forest\n\nbut-alas-it must go\n\n[The country needs energy, the country needs coal]\n\n\n\n\n\nWe said, it is a nice forest\n\n                        old forest\n\n                        twelve-thousand-year-old forest\n\nand go it cannot\n\n\n\nEight years of blockading that past tense\n\nuntil we make a different necessity visible\nEvolution is the capacity to remember what has helped[1]",
         references: "[1] Rudolf Jurolek, Bukolika (Modrý Peter, 2021)",
-        performers: "Anna Čonková, Vojtěch Šembera",
+        performers: "Ivana Chrapková",
     },
     {
         sk: {
@@ -181,7 +181,7 @@ export const trees = [
         },
         poem: "To Whom It May Concern:\n\nThis is a request for refuge.\n\n\n\nTo Whom It May Concern:\n\nThe gates will open to the tree.\n\n\n\nFloating, roots covered in soil and cloth, balanced on a boat, on a truck,\n\nFloating, leaving, towards an arboretum perhaps, gates open, I enter\n\nwhat I left: condition incompatible with life (my life, and your life)\n\na storm blowing from paradise, forest burning\n\n\n\nAround my roots look for my resin, my animé\n\nthat will heal your lungs\n\nthat will heal your bronchitis\n\nthat will heal your inflammation\n\n\n\nwhat I left: the forest a war zone, condition incompatible with life\n\nThe deep truths sedimented in our syntax[1], I carry some of the sediment around my roots\n\n\n\nThe refugee tree, the first tree granted asylum",
         references: "[1] Klaus in Ben Lerner, Topeka School (Granta, 2020)",
-        performers: "Peter Mazalán",
+        performers: "Ivana Chrapková, Marek Kundlák",
     },
     {
         sk: {
@@ -199,6 +199,6 @@ export const trees = [
             story: "Lebanon's ancient cedar forests are home to trees more than 1,000 years old, but they face a critical threat from climate change. Rising temperatures are pushing cedars to higher altitudes for reproduction. Currently, they grow only in limited areas in reserves such as the Barouk Forest. Scientists predict that by 2100 cedars may only thrive at the northern tip of Lebanon.\n\nLebanese Cedars also battle insect infestations linked to a warming, drying climate. As temperatures rise, cedrus libani no longer thrives in its natural region 1,400–2,000 metres above sea level. Nine of the twelve existing cedar forests are already located on summits, so only three may escape heat by migrating higher. By 2100, only a few cedar forests may remain in a small northern section of Mount Lebanon.",
         },
         poem: "shorter winters\n\nthe words spoken and then withdrawn\n\nonly their afterklang to accompany us\n\nsomething spoken and then withdrawn\n\na taste that hovers midair\n\n\n\nCedars need a minimum amount of snow to regenerate\n\nto need snow but just a little\n\nthe cones open up in the cold\n\nbut there is no further up for them to go\n\n\n\nshorter winters\n\nto say and then withdraw the said\n\nonly its afterklang to accompany us",
-        performers: "Vojtěch Šembera",
+        performers: "Peter Mazalán",
     },
 ];
